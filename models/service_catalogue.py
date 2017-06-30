5e23aa23121b634bd9e821b63caf9645b1c51edb
Hugues <jean.hugues@appy.solutions>
2017-06-30 09:22:50 +0300

    Retirer rôle & days
    - Retirer la notion de rôle car ambugüe
    - Retirer days sur les tâches

--- models/service_catalogue.py
+++ models/service_catalogue.py
@@ -9,14 +9,10 @@ class ProjectTask(models.Model):
 	"""docstring for project_task"""
 	_inherit = "project.task"
 
-	# days = fields.Selection([('1', '1'),
-	# 						('2', '2'),
-	# 						('3', '3'),
-	# 						('4', '4'),
-	# 						('5', '5')
-	# 						], 'Days', default='1')
 	details = fields.Text('Details')
 	catalogue_id = fields.Many2one('service.catalogue', 'Service Catalogue')
+	assign_id = fields.Many2one('service.catalogue.assign', 'Assignation')
+
 
 class ServiceCatalogue(models.Model):
 	_name = "service.catalogue"
@@ -26,11 +22,6 @@ class ServiceCatalogue(models.Model):
 	role_id = fields.Many2one('service.catalogue.role', 'Role')
 	task_ids = fields.One2many('project.task', 'catalogue_id', 'Tasks')
 	assing_ids = fields.One2many('service.catalogue.assign', 'catalogue_id', 'Assignation')
-	# role_count = fields.Integer('Role Catalogue count', compute='compute_role_count')
-	# visibility = fields.Selection([('owner', 'Owner'),
-	# 								('follower', 'Follower'),
-	# 								('all', 'All')
-	# 								], 'Visibility', default='owner')
 
 	# @api.one
 	# def compute_role_count(self):
@@ -44,17 +35,6 @@ class ServiceCatalogue(models.Model):
 			self.visibility = 'all'
 
 
-# class ServiceCatalogueRole(models.Model):
-# 	"""docstring for service_catalogue_role"""
-# 	_name = "service.catalogue.role"
-
-# 	name = fields.Char('Name', required=True, translate=True)
-# 	active = fields.Boolean(default=True)
-# 	description = fields.Text('Description')
-# 	catalogue_ids = fields.One2many('service.catalogue', 'role_id', 'Service Catalogue')
-# 	assign_ids = fields.One2many('service.catalogue.assign', 'role_id', 'Assignation')
-
-
 class ServiceCatalogueAssign(models.Model):
 	"""docstring for service_catalogue_assign"""
 	_name = "service.catalogue.assign"
@@ -64,6 +44,7 @@ class ServiceCatalogueAssign(models.Model):
 	# role_id = fields.Many2one('service.catalogue.role', 'Role')
 	catalogue_id = fields.Many2one('service.catalogue', 'Service Catalogue')
 	user_id = fields.Many2one('res.users', 'Assign to')
+	task_ids = fields.One2many('project.task', 'catalogue_id', 'Tasks')
 
 	@api.model
 	def _default_date(self):
@@ -71,3 +52,11 @@ class ServiceCatalogueAssign(models.Model):
 		return current_date
 
 	date_begin = fields.Date('Begin date', default=_default_date, index=True)
+
+
+class StandardRoutine(models.Model):
+	"""docstring for StandardRoutine"""
+	
+	_name = 'standard.routine'
+	_inherit = 'project.project'
+
